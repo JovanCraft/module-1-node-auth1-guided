1e63aab469fff1ce452f3ca323735c5381427bf5
jovan <jovancraft@gmail.com>
2024-02-12 14:22:25 -0600

    successfully configured the register endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,17 +1,28 @@
 const express = require('express')
-
+const bcrypt = require('bcryptjs')
+const User = require('../users/users-model')
 const router = express.Router()
 
 
 router.post('/register', async (req, res, next) => {
-    res.json({ message: 'register working!'})
+    try {
+    const { username, password } = req.body
+    const hash = bcrypt.hashSync(password, 8)//8 means it will be done 2 to the 8th power(2 ^ 8)!!!
+    const newUser = { username, password: hash }
+    const result = await User.add(newUser)
+    res.status(201).json({
+        message: `nice to have you on, ${result.username}`
+    })
+    } catch(err) {
+        next(err)
+    }
 })
 
 router.post('/login', async (req, res, next) => {
     res.json({ message: 'login working!' })
 })
 
-router.get('/logout', async (req, res, next) => { 
+router.get('/logout', async (req, res, next) => {
     res.json({ message: 'logout working!' })
 })
 
