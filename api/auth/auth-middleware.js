5c0ee03d7b293d6721d02fbfe471825aa31f493b
jovan <jovancraft@gmail.com>
2024-02-14 15:04:52 -0600

    finished

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -9,6 +9,8 @@ async function protect(req, res, next) {
 
 //to delete the Cookie by hand, go to Application tab inside the chrome dev console, highlight the cookie and press the x. This effectively takes it off and gives the message you shall not pass at the bottom of the screen instead of the users
 
+//DONE!
+
 
 module.exports = {
     protect
